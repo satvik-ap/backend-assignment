e12c5749206fe3906d837f54a03f5c0f5d64186b
satvik_ap <satvikap22@gmail.com>
2026-08-20 21:00:52 +0530

    Update and rename introduction (1).js to introduction.js

--- Assignment 03/introduction (1).js
+++
@@ -1,4 +0,0 @@
-console.log("Name: Yash Tambade");
-console.log("Education: B.Tech");
-console.log("Current Learning: Node.js");
-console.log("Goal: Web dev.");
---
+++ Assignment 03/introduction.js
@@ -0,0 +1,4 @@
+console.log("Name: Satvik ap");
+console.log("Education: B.Tech cse");
+console.log("Current Learning: Node.js");
+console.log("Goal: Web developer.");
